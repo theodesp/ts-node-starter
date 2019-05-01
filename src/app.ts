c35f60593f9fe9855236a9175f03d1c267787ec5
Theo <theo.despoudis@teckro.com>
2019-05-01 19:38:10 +0100

    added winston logger

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,7 @@
-console.warn("Hello");
+import dotenv from 'dotenv';
+import logger from './core/logger';
+
+// Load environment variables from .env file, where API keys and passwords are configured
+dotenv.config({ path: '.env.example' });
+
+logger.warn('Hey');
