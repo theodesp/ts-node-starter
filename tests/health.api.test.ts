2730b299b2a6ff0c5b14150676f870c4c27ce17a
Theo <theo.despoudis@teckro.com>
2019-05-02 16:46:04 +0100

    added initial tests

---
+++ tests/health.api.test.ts
@@ -0,0 +1,9 @@
+import request from 'supertest';
+import app from '../src/app';
+
+describe('/health', () => {
+    it('returns 200 OK', () => {
+        return request(app).get('/health')
+            .expect(200);
+    });
+});
