25d340ff36c727c4d4a0255021230e6af28b3323
Theo <theo.despoudis@teckro.com>
2019-05-03 10:34:16 +0100

    add more middleware and fixed assets

--- src/core/middleware.ts
+++ src/core/middleware.ts
@@ -1,14 +1,20 @@
 import parser from 'body-parser';
 import compression from 'compression';
 import cors from 'cors';
+import path from 'path';
 import {Router} from 'express';
 import expressValidator from 'express-validator';
 import lusca from 'lusca';
 import session from 'express-session';
+import paginate from 'express-paginate';
+import favicon from 'serve-favicon';
 import {SESSION_SECRET} from './settings';
 
 export const handleCors = (router: Router) =>
     router.use(cors({credentials: true, origin: true}));
+
+export const handlePagination = (router: Router) =>
+    router.use(paginate.middleware(10, 50));
 
 export const handleValidation = (router: Router) =>
     router.use(expressValidator());
@@ -40,12 +46,18 @@ export const handleSecurity = (router: Router) => {
     }));
 };
 
+export const handleFavicon = (router: Router) => {
+    router.use(favicon(path.join(__dirname, '../public', 'favicon.ico')));
+};
+
 export default [
+    handlePagination,
     handleSession,
     handleValidation,
     handleCors,
     handleBodyRequestParsing,
     handleCompression,
-    handleSecurity
+    handleSecurity,
+    handleFavicon,
 ];
 
